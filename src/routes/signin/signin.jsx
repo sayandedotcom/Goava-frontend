a2d6d974590ca80f93d32c3b9faeb3a4eef071c7
Sayan De <sayandeten@gmail.com>
2023-03-18 15:12:33 +0530

    Adding form validation in signin

--- src/routes/signin/signin.jsx
+++ src/routes/signin/signin.jsx
@@ -5,8 +5,39 @@ import Google from "../../assests/svg-google.svg";
 import Facebook from "../../assests/svg-facebook.svg";
 import FormInput from "../../components/input/input";
 import NewTooltip from "../../components/tooltip/tooltip";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    email: yup
+      .string()
+      .required("Email is required")
+      .matches(/\S+@\S+\.\S+/, "Email is not valid"),
+    password: yup
+      .string()
+      .required("Password is required")
+      .min(6, "Password must be less than 6 character"),
+  })
+  .required();
 
 const Signin = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+  console.log(errors);
+
   return (
     <div className="flex flex-col justify-center items-center h-auto mb-24 gap-4">
       <p className="text-4xl font-black uppercase">Sign in</p>
@@ -19,18 +50,21 @@ const Signin = () => {
       <div className="flex flex-col gap-3 mt-5 text-center font-semibold 	w-80">
         <p>Sign in with your Email and Password</p>
         <hr className="border-1 border-black" />
-        <form className="flex flex-col gap-6">
+
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
           <FormInput
             placeholder="Email Address"
             type="email"
             name="email"
-            required
+            register={{ ...register("email") }}
+            errorMessage={errors.email?.message}
           />
           <FormInput
             placeholder="Password"
             type="password"
             name="password"
-            required
+            register={{ ...register("password") }}
+            errorMessage={errors.password?.message}
           />
           <Button buttonType="inverted" type="submit">
             Sign In
