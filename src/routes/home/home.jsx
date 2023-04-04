99ec7b3ae3743aa5a35c10abea823ad5f2871510
Sayan De <sayandeten@gmail.com>
2023-04-04 16:10:17 +0530

    adding jsconfig file

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { fullProgress } from "../../redux/loaderSlice";
-import Bannercarousel from "../../components/homeComponents/bannerCarousel/bannerCarousel";
-import CardsCarousel from "../../components/homeComponents/cardsCarousel/cardsCarousel";
+import { fullProgress } from "redux/loaderSlice";
+import Bannercarousel from "components/homeComponents/bannerCarousel/bannerCarousel";
+import CardsCarousel from "components/homeComponents/cardsCarousel/cardsCarousel";
 
 const Home = () => {
   const dispatch = useDispatch();
