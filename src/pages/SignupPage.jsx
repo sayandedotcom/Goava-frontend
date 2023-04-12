851fde45535e0aa5fc00bb3c50d499ef3d787673
Sayan De <sayandeten@gmail.com>
2023-04-13 01:22:57 +0530

    yup

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import {useForm} from 'react-hook-form';
 import {yupResolver} from '@hookform/resolvers/yup';
-import * as yup from 'yup';
 import Button from 'components/button';
 import Google from 'assests/svg-google.svg';
 import FormInput from 'components/formInput';
