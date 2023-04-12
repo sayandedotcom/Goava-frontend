15b558014d03fb37f930731583efd30d66616324
Sayan De <sayandeten@gmail.com>
2023-04-12 21:03:10 +0530

    small navbar

--- src/layout/navbar/smallnavbar/smallnavbar.jsx
+++ src/layout/navbar/smallnavbar/smallnavbar.jsx
@@ -26,37 +26,38 @@ const SmallNavbar = () => {
       </nav>
       <div
         className={`absolute ${
-          open ? 'top-[96px]' : 'top-[-96px] hidden'
-        }  flex w-full flex-col items-center justify-around gap-5 bg-slate-400 py-4 md:hidden`}>
+          open ? 'top-[98px]' : 'top-[-100px] hidden'
+        }  flex w-full flex-col items-center justify-between bg-slate-400 md:hidden`}>
         {navbarLists.map(
           ({id, name, component, route}) =>
             id <= 3 && (
-              <NewTooltip key={id} title={name}>
-                <Link
-                  onClick={() => setOpen(!open)}
-                  className='font-semibold text-black no-underline'
-                  to={route}>
-                  {component}
-                  <span>{name}</span>
-                </Link>
-              </NewTooltip>
+              <Link
+                key={id}
+                onClick={() => setOpen(!open)}
+                className='flex w-full items-center justify-center gap-2 py-2 font-semibold text-black no-underline hover:bg-slate-500'
+                to={route}>
+                {component}
+                <span>{name}</span>
+              </Link>
             )
         )}
         {navbarLists.map(
           ({id, name, component, route}) =>
             id > 3 && (
-              <NewTooltip key={id} title={name}>
-                <Link onClick={() => setOpen(!open)} to={route}>
-                  <span className='relative'>
-                    {component}
-                    <span className='absolute	top-[-13px] right-[-4px] flex h-5 w-5 items-center justify-center rounded-[50%] bg-red-600 text-xs font-light text-white'>
-                      <Countup end={15} duration={4} />
-                    </span>
+              <Link
+                key={id}
+                onClick={() => setOpen(!open)}
+                to={route}
+                className='flex w-full items-center justify-center gap-2 py-2 font-semibold text-black no-underline hover:bg-slate-500'>
+                <span className='relative'>
+                  {component}
+                  <span className='absolute	top-[-13px] right-[-4px] flex h-5 w-5 items-center justify-center rounded-[50%] bg-red-600 text-xs font-light text-white'>
+                    <Countup end={15} duration={4} />
                   </span>
+                </span>
 
-                  <span>{name}</span>
-                </Link>
-              </NewTooltip>
+                <span>{name}</span>
+              </Link>
             )
         )}
       </div>
